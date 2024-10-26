999c67625cdcdbe1781687e76ca4e6eab64ab470
Paulo Almeida <109768075+p-almeida12@users.noreply.github.com>
2024-10-26 12:06:16 +0100

    config data module

--- twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,12 +1,13 @@
 package com.microservices.twitter.to.kafka.service;
 
-import com.microservices.twitter.to.kafka.service.config.TwitterToKafkaServiceConfigData;
+import com.microservices.config.TwitterToKafkaServiceConfigData;
 import com.microservices.twitter.to.kafka.service.runner.StreamRunner;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import twitter4j.TwitterException;
 
 import java.util.Arrays;
@@ -14,6 +15,7 @@ import java.util.Arrays;
 @Slf4j
 @SpringBootApplication
 @RequiredArgsConstructor
+@ComponentScan(basePackages = "com.microservices")
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     private final StreamRunner streamRunner;
