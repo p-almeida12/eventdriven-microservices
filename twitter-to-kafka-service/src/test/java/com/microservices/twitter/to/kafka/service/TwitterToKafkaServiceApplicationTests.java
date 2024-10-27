2e10e9b5afc633fe614825e23b6593f10eb7d2a4
Paulo Almeida <109768075+p-almeida12@users.noreply.github.com>
2024-10-27 15:10:04 +0000

    config server

--- twitter-to-kafka-service/src/test/java/com/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
+++ twitter-to-kafka-service/src/test/java/com/microservices/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -1,6 +1,6 @@
 package com.microservices.twitter.to.kafka.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
