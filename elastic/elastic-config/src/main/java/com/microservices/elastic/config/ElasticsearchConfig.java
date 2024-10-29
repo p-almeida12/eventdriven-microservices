026d12f8d0862f18e1b7c5232c00ace167cd0831
Paulo Almeida <109768075+p-almeida12@users.noreply.github.com>
2024-10-29 13:47:41 +0000

    kafka consumer with elasticsearch

---
+++ elastic/elastic-config/src/main/java/com/microservices/elastic/config/ElasticsearchConfig.java
@@ -0,0 +1,50 @@
+package com.microservices.elastic.config;
+
+import com.microservices.config.ElasticConfigData;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Objects;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableElasticsearchRepositories(basePackages = "com.microservices.elastic.index.client.repository")
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+
+    private final ElasticConfigData elasticConfigData;
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        UriComponents serverUri = UriComponentsBuilder.fromHttpUrl(elasticConfigData.getConnectionUrl()).build();
+        return new RestHighLevelClient(
+                RestClient.builder(new HttpHost(
+                        Objects.requireNonNull(serverUri.getHost()),
+                        serverUri.getPort(),
+                        serverUri.getScheme()
+                )).setRequestConfigCallback(
+                        requestConfigBuilder ->
+                                requestConfigBuilder
+                                        .setConnectTimeout(elasticConfigData.getConnectionTimeoutMs())
+                                        .setSocketTimeout(elasticConfigData.getSocketTimeoutMs())
+
+                )
+        );
+    }
+
+    @Bean
+    public ElasticsearchOperations elasticsearchOperations() {
+        return new ElasticsearchRestTemplate(elasticsearchClient());
+    }
+
+}
---
+++ elastic/elastic-index-client/src/main/java/com/microservices/elastic/index/client/service/impl/TwitterElasticIndexClient.java
@@ -0,0 +1,40 @@
+package com.microservices.elastic.index.client.service.impl;
+
+import com.microservices.config.ElasticConfigData;
+import com.microservices.elastic.index.client.service.ElasticIndexClient;
+import com.microservices.elastic.index.client.util.ElasticIndexUtil;
+import com.microservices.elastic.model.index.impl.TwitterIndexModel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.IndexedObjectInformation;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.IndexQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "elastic-config.is-repository", havingValue = "false")
+public class TwitterElasticIndexClient implements ElasticIndexClient<TwitterIndexModel> {
+
+    private final ElasticConfigData elasticConfigData;
+    private final ElasticsearchOperations elasticsearchOperations; //index and query against elasticsearch
+    private final ElasticIndexUtil<TwitterIndexModel> elasticIndexUtil;
+
+    @Override
+    public List<String> save(List<TwitterIndexModel> documents) {
+        List<IndexQuery> indexQueries = elasticIndexUtil.getIndexQueries(documents);
+        List<String> documentIds = elasticsearchOperations.bulkIndex(
+                indexQueries,
+                IndexCoordinates.of(elasticConfigData.getIndexName())
+        ).stream().map(IndexedObjectInformation::getId).collect(Collectors.toList());
+        log.info("Documents indexed successfully with type: {} and ids: {}", TwitterIndexModel.class.getName(),
+                documentIds);
+        return documentIds;
+    }
+}
