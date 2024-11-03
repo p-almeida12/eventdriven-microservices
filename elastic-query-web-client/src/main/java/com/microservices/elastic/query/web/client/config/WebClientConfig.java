dc22ea5e24d1425373266a4266d8d328adac61bc
Paulo Almeida <109768075+p-almeida12@users.noreply.github.com>
2024-11-03 19:21:52 +0000

    elastic query web client with keycloak and docker compose update

--- elastic-query-web-client/src/main/java/com/microservices/elastic/query/web/client/config/WebClientConfig.java
+++ elastic-query-web-client/src/main/java/com/microservices/elastic/query/web/client/config/WebClientConfig.java
@@ -7,12 +7,15 @@ import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
@@ -24,23 +27,34 @@ import java.util.concurrent.TimeUnit;
 public class WebClientConfig {
 
     private final ElasticQueryWebClientConfigData.WebClient elasticQueryWebClientConfigData;
-    private final UserConfigData userConfigData;
 
-    public WebClientConfig(ElasticQueryWebClientConfigData webClientConfigData, UserConfigData userData) {
+    @Value("${security.default-client-registration-id}")
+    private String defaultClientRegistrationId;
+
+    public WebClientConfig(ElasticQueryWebClientConfigData webClientConfigData) {
         this.elasticQueryWebClientConfigData = webClientConfigData.getWebClient();
-        this.userConfigData = userData;
     }
 
     @LoadBalanced
     @Bean("webClientBuilder")
-    WebClient.Builder webClientBuilder() {
+    WebClient.Builder webClientBuilder(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository
+    ) {
+
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+                        clientRegistrationRepository,
+                        oAuth2AuthorizedClientRepository);
+        oauth2.setDefaultOAuth2AuthorizedClient(true);
+        oauth2.setDefaultClientRegistrationId(defaultClientRegistrationId);
+
         return WebClient.builder()
-                .filter(ExchangeFilterFunctions
-                        .basicAuthentication(userConfigData.getUsername(), userConfigData.getPassword()))
                 .baseUrl(elasticQueryWebClientConfigData.getBaseUrl())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, elasticQueryWebClientConfigData.getContentType())
                 .defaultHeader(HttpHeaders.ACCEPT, elasticQueryWebClientConfigData.getAcceptType())
                 .clientConnector(new ReactorClientHttpConnector(HttpClient.from(getTcpClient())))
+                .apply(oauth2.oauth2Configuration())
                 .codecs(clientCodecConfigurer ->
                         clientCodecConfigurer
                                 .defaultCodecs()
@@ -59,4 +73,5 @@ public class WebClientConfig {
                                     TimeUnit.MILLISECONDS));
                 });
     }
+
 }
